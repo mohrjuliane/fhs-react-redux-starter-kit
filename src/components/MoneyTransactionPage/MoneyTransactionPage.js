2ee36856699ce39e3d579d8b44ef710e0434da06
Juliane Mohr <julianemohr@hotmail.com>
2022-04-03 16:25:23 +0200

    disable .env and add firebase-config.js

--- src/components/MoneyTransactionPage/MoneyTransactionPage.js
+++ src/components/MoneyTransactionPage/MoneyTransactionPage.js
@@ -1,43 +1,43 @@
 import React, { useEffect, useState } from 'react'
 import { MoneyTransactionCreate } from '../MoneyTransactionCreate/MoneyTransactionCreate'
 import { MoneyTransactionList } from '../MoneyTransactionList/MoneyTransactionList'
+import { db } from '../../firebase-config'
+import { collection, getDocs } from 'firebase/firestore'
 
 export const MoneyTransactionPage = () => {
   const [moneyTransactions, setMoneyTransactions] = useState([])
   const [users, setUsers] = useState([{}])
+  // const [ownId] = useState('7ksRNdhVDN8McZVloTEc')
+  const userCollectionRef = collection(db, 'users')
+  const transactionCollectionRef = collection(db, 'transactions')
 
   useEffect(() => {
-    fetch('http://localhost:3001/user')
-      .then((response) => response.json())
-      .then((json) => setUsers(json))
-    fetch('http://localhost:3001/money-transaction')
-      .then((response) => response.json())
-      .then((json) => setMoneyTransactions(json))
+    getUsers()
+    getTransactions()
   }, [])
 
-  function handleSubmit (creditor, debitor, amount) {
-    fetch('http://localhost:3001/user')
-      .then((response) => response.json())
-      .then((json) => console.log(json))
+  async function getUsers () {
+    const data = await getDocs(userCollectionRef)
+    const parsedData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+    console.log(parsedData)
+    setUsers(parsedData)
+  }
 
-    const newTransaction =
+  async function getTransactions () {
+    const data = await getDocs(transactionCollectionRef)
+    const parsedData = data.docs.map((doc) => ({ ...doc.data() }))
+    console.log(parsedData)
+    setMoneyTransactions(parsedData)
+  }
+
+  function handleSubmit (creditor, debitor, amount) {
+    /* const newTransaction =
             ({
               creditorId: creditor,
               debitorId: debitor,
               amount: amount,
               paidAt: null
-            })
-    fetch('http://localhost:3001/money-transaction', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(newTransaction)
-    })
-      .then(res => res.json())
-      .then(json => console.log(json))
-
-    fetch('http://localhost:3001/money-transaction')
-      .then((response) => response.json())
-      .then((json) => setMoneyTransactions(json))
+            }) */
   }
 
   return (
