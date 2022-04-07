33a2d3508004e504cd64bf696e536b3f49eff7e4
Juliane Mohr <julianemohr@hotmail.com>
2022-04-07 14:02:48 +0200

    Add navigate to money-transactions

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -4,18 +4,23 @@ import { InputField } from '../InputField/InputField'
 import { Button } from '../Button/Button'
 import { useFormik } from 'formik'
 import { object, string } from 'yup'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { auth, db } from '../../firebase-config'
 import { doc, setDoc } from 'firebase/firestore'
+import * as Yup from 'yup'
 
 const userSchema = object({
   email: string().email('Invalid Email').required('Required'),
-  password: string().min(5).required('Required')
+  password: string().min(5).required('Required'),
+  passwordConfirmation: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match')
 })
 
 export const SignUp = ({ user, onUpdateUser }) => {
   const [loginError, setError] = useState()
+  const navigate = useNavigate()
+
   const formik = useFormik({
     initialValues: user,
     validationSchema: userSchema,
@@ -25,12 +30,12 @@ export const SignUp = ({ user, onUpdateUser }) => {
   async function handleSubmit (name, email, password) {
     try {
       const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
-
       const uid = userCredentials.user.uid
       await setDoc(doc(db, 'users', uid), { name: name })
     } catch (error) {
       setError(error.message)
     }
+    navigate('/money-transactions')
   }
 
   return (
@@ -60,6 +65,14 @@ export const SignUp = ({ user, onUpdateUser }) => {
           value={formik.values.password}
         />
         {<div className={styles.errorMessage}>{formik.errors.password}</div>}
+        <InputField
+            type="password"
+            name="passwordConfirmation"
+            title="Re-enter password"
+            onChange={formik.handleChange}
+            value={formik.values.passwordConfirmation}
+        />
+        {<div className={styles.errorMessage}>{formik.errors.passwordConfirmation}</div>}
         {<div className={styles.errorMessage}>{loginError}</div>}
         <Button type="submit" isPrimary={true}>
           Sign Up
