cfd99bcee520c8898acb28360308c9febae29be4
Juliane Mohr <julianemohr@hotmail.com>
2022-04-13 10:19:00 +0200

    3rd Performance issue: useCallback for finding Username with id function

--- src/components/MoneyTransactionList/MoneyTransactionList.js
+++ src/components/MoneyTransactionList/MoneyTransactionList.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { TableRow } from '../TableRow/TableRow'
 
 const DEFAULT_USERS = []
 const DEFAULT_TRANSACTIONS = []
 
 export const MoneyTransactionList = ({ moneyTransactions = DEFAULT_TRANSACTIONS, users = DEFAULT_USERS, ownId, updateDocument }) => {
-  function findNameById (id) {
+  const findNameById = useCallback((id) => {
     const user = users.find((x) => x.id === id)
     if (user !== undefined) {
       return user.name
     } else {
       return '<deleted user>'
     }
-  }
+  }, [users])
+
   return (
     <>
       {users !== undefined && moneyTransactions.map((element, key) => {
