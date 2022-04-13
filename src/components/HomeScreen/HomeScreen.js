ec5516b5e67da899c60d02690a411e88be7ba2de
Juliane Mohr <julianemohr@hotmail.com>
2022-04-13 08:53:31 +0200

    Remove delete user

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -1,20 +1,10 @@
 import React from 'react'
-import { Button } from '../Button/Button'
 import styles from './HomeScreen.module.css'
-import { Navigate } from 'react-router-dom'
-import { deleteUser } from 'firebase/auth'
 
 export const HomeScreen = ({ user }) => {
-  function deleteAccount () {
-    deleteUser(user).then(() => { return <Navigate to="/sign-in"></Navigate> })
-      .catch((error) => {
-        console.log(error)
-      })
-  }
   return (
     <div className={styles.homeContainer}>
       <h1>Welcome on your Money Organizer! </h1>
-      {user && <Button onClick={deleteAccount}>Delete account</Button>}
     </div>
   )
 }
